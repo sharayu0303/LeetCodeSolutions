4b840a371480a6831d457880fa91648f5057f996
Sharayu Mujumale <sharayu0303@gmail.com>
2024-10-05 18:40:06 +0530

    FirstCommit

---
+++ LeetCodeProblems/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCodeProblems
+{
+    public class Program
+    {
+        //TwoSum
+        public static int[] nums = {3,3};
+        public static int target = 6;
+
+        //IsPalindrome
+        static int x = 121;
+
+        //LargestCommonPrefix
+        public static string[] strs = { "flower", "flow", "flight" };
+
+
+        static void Main(string[] args)
+        {
+            //TwoSum
+            new Solutions().TwoSum(nums, target);
+
+            //Palindrome
+            new Solutions().IsPalindrome(x);
+
+            //LargestCommonPrefix
+            new Solutions().LongestCommonPrefix(strs);
+        }
+    }
+}
+
+
+
---
+++ LeetCodeProblems/Solutions.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCodeProblems
+{
+    class Solutions
+    {
+        public int[] TwoSum(int[] nums, int target)
+        {
+            List<int> result = new List<int>();
+            for (int i = 0; i< nums.Length; i++)
+            {
+                for (int j = i+1; j < nums.Length; j++)
+                {
+                    if (i != j)
+                    {
+                        if (nums[i] + nums[j] == target)
+                        {
+                            result.Add(i);
+                            result.Add(j);
+                        }
+                    } 
+                   
+                }
+            }
+            return result.ToArray();
+        }
+
+        public bool IsPalindrome(int x)
+        {
+            int original = x;
+            int reverse = 0;
+            if(x < 0)
+            {
+                return false;
+            }
+            while (x > 0)
+            {
+                int remainder = x % 10;
+                reverse = reverse * 10 + remainder;
+                x = x / 10;
+            }
+
+                if (original == reverse)
+                {
+                    return true;
+                }
+                else
+                {
+                    return false;
+                }
+            
+        }
+
+        public string LongestCommonPrefix(string[] strs)
+        {
+           int minstrLength = strs.Min(str => str.Length);
+            string a = "";
+            for (int i = 0;i < minstrLength;i++)
+            {
+                char firstString = strs[0][i];
+                bool allMatch = true;
+
+                for (int j = 1; j < strs.Length; j++)
+                {
+                    if (strs[j][i] != firstString)
+                    {
+                        allMatch = false;
+                        break;
+                    }
+                }
+                if (allMatch)
+                {
+                    a += firstString.ToString();
+                }
+                else
+                {
+                    break;
+                }
+            }
+            return a;
+        }
+    }
+}
